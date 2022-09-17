0c69edcecc7b39edbecddac77c47a7d295e5d5e5
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-09-17 16:16:12 +0900

    feat: カウンター保存処理実装

--- hasura-practice/src/pages/index.tsx
+++ hasura-practice/src/pages/index.tsx
@@ -3,15 +3,51 @@ import { useRecoilState } from 'recoil'
 import { countersState } from '../features/counters/atom'
 import { useState } from 'react'
 import { useCountersQuery } from '../features/counters/hooks/useCountersQuery'
+import { gql, useMutation } from '@apollo/client'
 
 export const Index = () => {
   const [counters, setCounters] = useRecoilState(countersState)
   const [counterId, setCounterId] = useState(1)
   const { loading, error } = useCountersQuery()
 
+  const CREATE_COUNTERS = gql`
+    mutation ($counter: counter_insert_input!) {
+      insert_counter(
+        objects: [$counter]
+        on_conflict: { constraint: counter_pkey, update_columns: [name, count, isDeleted] }
+      ) {
+        returning {
+          id
+          name
+          count
+          isDeleted
+        }
+      }
+    }
+  `
+
+  const [createCounter, { loading: saveLoading }] = useMutation(CREATE_COUNTERS)
+
   function addCounter() {
     setCounters([...counters, { id: counterId + 1, name: 'new', count: 0 }])
     setCounterId(counterId + 1)
+  }
+
+  async function handleClick() {
+    await Promise.all(
+      counters.map(async (counter) => {
+        await createCounter({
+          variables: {
+            counter: {
+              id: counter.id,
+              name: counter.name,
+              count: counter.count,
+              isDeleted: false,
+            },
+          },
+        })
+      }),
+    )
   }
 
   // TODO: suspenseの仕組みを調べてコード改善
@@ -25,6 +61,9 @@ export const Index = () => {
       ))}
       <button className=' bg-green-400 mt-4 text-white p-1 w-full' onClick={addCounter}>
         new counter
+      </button>
+      <button className=' bg-blue-400 mt-4 text-white p-1 w-full' onClick={handleClick}>
+        save counter
       </button>
       {<p>title list: {counters.map((counter) => counter.name).join(',')}</p>}
       {<p>sum of count: {counters.reduce((current, counter) => current + counter.count, 0)}</p>}
